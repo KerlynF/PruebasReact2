f3e99b4216b029989832461399080c563d085b54
Kerlyn <kerlynmanuel1@gmail.com>
2022-06-15 15:48:52 -0400

    last change

--- contador/src/App.js
+++ contador/src/App.js
@@ -1,25 +1,31 @@
 import './App.css';
 import Button from './Components/Button';
 import logoViva from './images/R.jpg';
+import ClickCounter from './Components/Counter';
+import {useState} from 'react';
 
 
 function App() {
 
   const beginCounter = () => 
   {
-    console.log("Begin");
+    setNumClicks(numClicks + 1);
   }
+
+  const [numClicks, setNumClicks] = useState(0);
 
   const restartCounter = () =>
   {
-    console.log("Restarting");
+    setNumClicks(0);
   }
   return (
     <div className="App">
       <div className='logo-container'>
           <img className='logo-viva' src={logoViva} alt='Logo de Viva'/>
       </div>
+      
       <div className='counter-container'>
+        <ClickCounter numClicks={numClicks}/>
         <Button text='Click to begin' isButtonClick={true} manageClick={beginCounter}/>
         <Button text='Click to restart' isButtonClick={false} manageClick={restartCounter}/>
       </div>
--- contador/src/Components/Counter.js
+++ contador/src/Components/Counter.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import '../styleSheets/Counter.css'
+function ClickCounter( {numClicks} )
+{
+    return (
+        <div className='counter-class'>
+            {numClicks}
+        </div>
+    );
+}
+
+export default ClickCounter;
